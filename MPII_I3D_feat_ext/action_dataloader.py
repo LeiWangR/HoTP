70eb45b681b8ac64f907f973b6529ed73b6ac7f4
Lei Wang <u7014095@anu.edu.au>
2023-07-16 22:49:05 +1000

    Add files via upload

---
+++ MPII_I3D_feat_ext/action_dataloader.py
@@ -0,0 +1,369 @@
+import random
+import torch
+from torch.utils import data
+import pandas as pd
+import os
+import glob
+import math
+import numpy as np
+from skimage import io, transform, img_as_float
+import numpy as np
+from torchvision import transforms, utils
+from skimage.transform import resize
+
+
+'''
+Researcher: Lei Wang
+This is the data loading and processing using pytorch for UCF101 and HMDB51 dataset
+
+Some explanations:
+desired_frames = 64
+The input for the deep learning pipeline is RGB videos and optical flow videos
+The desired frames for both videos are all 64
+We resize the video to 256x256
+ For training, we random crop to get a square video 224x224, also horizontal flip
+ For testing, we do center crop of the video only
+
+'''
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+    # Rescale here may be not necessary as each frame has been resized 
+    # when loading each eimage 
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size=(256, 256)):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        
+        video_rgb, video_opt, video_features, video_label = sample['video_rgb'], sample['video_opt'], sample['BOW'], sample['video_label']
+    
+        h_rgb, w_rgb = video_rgb.shape[1],video_rgb.shape[2]
+        desired_frames = video_rgb.shape[0]
+        h_opt, w_opt = video_opt.shape[1],video_opt.shape[2]
+        
+        if isinstance(self.output_size, int):
+            # for rgb video
+            if h_rgb > w_rgb:
+                new_h_rgb, new_w_rgb = self.output_size * h_rgb / w_rgb, self.output_size
+            else:
+                new_h_rgb, new_w_rgb = self.output_size, self.output_size * w_rgb / h_rgb
+            # for optic flow video
+            if h_opt > w_opt:
+                new_h_opt, new_w_opt = self.output_size * h_opt / w_opt, self.output_size
+            else:
+                new_h_opt, new_w_opt = self.output_size, self.output_size * w_opt / h_opt
+        else:
+            new_h_rgb, new_w_rgb = self.output_size
+            new_h_opt, new_w_opt = self.output_size
+
+        new_h_rgb, new_w_rgb = int(new_h_rgb), int(new_w_rgb)
+        new_video_rgb=np.zeros((desired_frames,new_h_rgb,new_w_rgb,3))
+        new_h_opt, new_w_opt = int(new_h_opt), int(new_w_opt)
+        new_video_opt=np.zeros((desired_frames,new_h_opt,new_w_opt,2))
+        for i in range(desired_frames):
+            # rgb videos
+            image=video_rgb[i,:,:,:]
+            img = transform.resize(image, (new_h_rgb, new_w_rgb), mode='constant')
+            new_video_rgb[i,:,:,:]=img
+            # optical flow
+            image=video_opt[i,:,:,:]
+            img = transform.resize(image, (new_h_opt, new_w_opt), mode='constant')
+            new_video_opt[i,:,:,:]=img
+            
+        return {'video_rgb': new_video_rgb, 'video_opt': new_video_opt, 'BOW': video_features, 'video_label': video_label}
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size=(224,224)):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        # desired_frames
+        video_rgb, video_opt, video_features, video_label = sample['video_rgb'], sample['video_opt'], sample['BOW'], sample['video_label']
+        # RGB and optical flow all with the same spatial scale
+        h, w = video_rgb.shape[1],video_rgb.shape[2]
+        new_h, new_w = self.output_size
+        desired_frames = video_rgb.shape[0]
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+         
+        new_video_rgb=np.zeros((desired_frames,new_h,new_w,3))
+        new_video_opt=np.zeros((desired_frames,new_h,new_w,2))
+        for i in range(desired_frames):
+            image=video_rgb[i,:,:,:]
+            image = image[top: top + new_h,left: left + new_w]
+            new_video_rgb[i,:,:,:]=image
+
+            image=video_opt[i,:,:,:]
+            image = image[top: top + new_h,left: left + new_w]
+            new_video_opt[i,:,:,:]=image
+
+        return {'video_rgb': new_video_rgb, 'video_opt': new_video_opt, 'BOW': video_features, 'video_label': video_label}
+
+class CenterCrop(object):
+    """Crop the given video at the center
+    This function is used for testing
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size=(224,224)):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        # 
+        video_rgb, video_opt, video_features, video_label = sample['video_rgb'], sample['video_opt'], sample['BOW'], sample['video_label']
+        # RGB and optical flow all with the same spatial scale
+        h, w = video_rgb.shape[1],video_rgb.shape[2]
+        new_h, new_w = self.output_size
+        desired_frames = video_rgb.shape[0]
+        top = int(np.round((h - new_h) / 2.))
+        left = int(np.round((w - new_w) / 2.))
+
+        new_video_rgb=np.zeros((desired_frames,new_h,new_w,3))
+        new_video_opt=np.zeros((desired_frames,new_h,new_w,2))
+        for i in range(desired_frames):
+            image=video_rgb[i,:,:,:]
+            image = image[top: top + new_h,left: left + new_w]
+            new_video_rgb[i,:,:,:]=image
+
+            image=video_opt[i,:,:,:]
+            image = image[top: top + new_h,left: left + new_w]
+            new_video_opt[i,:,:,:]=image
+
+        return {'video_rgb': new_video_rgb, 'video_opt': new_video_opt, 'BOW': video_features, 'video_label': video_label}
+
+
+class ToTensor(object):
+    def __call__(self, sample):
+        video_rgb, video_opt, video_label = sample['video_rgb'], sample['video_opt'], sample['video_label']
+        video_rgb = video_rgb.transpose((3, 0, 1, 2))
+        video_opt = video_opt.transpose((3, 0, 1, 2))
+        video_rgb = np.array(video_rgb)
+        video_opt = np.array(video_opt)
+        # video_label = [video_label]
+        
+        return {'video_rgb': torch.from_numpy(video_rgb), 'video_opt': torch.from_numpy(video_opt), 'video_label': torch.from_numpy(np.array(video_label))}
+
+
+class ActionDataset(data.Dataset):
+    def __init__(self, info_list, rgb_dir, opt_dir, bb_dir, mode='train', transforms = None):
+        self.info_list = pd.read_csv(info_list, header = None)
+        self.rgb_dir = rgb_dir
+        self.opt_dir = opt_dir
+        self.bb_dir = bb_dir
+        self.mode = mode
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.info_list)
+
+
+    def __getitem__(self, idx):
+        rgb_video_path = os.path.join(self.rgb_dir, self.info_list.iloc[idx, 1])
+        rgb_start = self.info_list.iloc[idx, 3]
+        opt_start = self.info_list.iloc[idx, 4]
+        sub_N = self.info_list.iloc[idx, 5]
+        stride = self.info_list.iloc[idx, 6]
+        sample = self.info_list.iloc[idx, 7]
+        
+        bb_dir = self.bb_dir
+        
+        # the class label should be (0, C-1), where C is the total number of classes
+        video_label = self.info_list.iloc[idx, 2]
+        # print(rgb_video_path, 'index: ', idx)
+        opt_x_video_path = os.path.join(self.opt_dir, 'u', self.info_list.iloc[idx, 1])
+        opt_y_video_path = os.path.join(self.opt_dir, 'v', self.info_list.iloc[idx, 1])
+        video_rgb = self.get_rgb_video(rgb_video_path, rgb_start, bb_dir, sub_N, stride, sample)
+        video_opt = self.get_opt_video(opt_x_video_path, opt_y_video_path, opt_start, bb_dir, sub_N, stride, sample)
+        # if it is in train mode, do horizontal flip for the video
+        # otherwise (in test mode), do not flip
+        # if self.mode == 'train':
+        #     # horizontal flip
+        #     if random.randint(1, 10) % 2 == 0:
+        #         video_rgb = self.left_right_flip(video_rgb)
+        #         video_opt = self.left_right_flip(video_opt)
+ 
+        sample = {'video_rgb': video_rgb, 'video_opt': video_opt, 'video_label': video_label}
+
+        if self.transforms:
+            sample = self.transforms(sample)
+        return sample
+
+    def get_rgb_video(self, rgb_video_path, rgb_start, bb_dir, sub_N, stride, sample):
+        # define the number of frames needed
+        desired_frames = sub_N
+        # print(sub_N)
+        start_point = int(rgb_start)
+        # the following parameters are fixed for experiments
+        height = 224
+        width = 224
+        
+        head, tail = os.path.split(rgb_video_path)
+        bb_file = tail + '.pth'
+        bb_tensor = torch.load(os.path.join(bb_dir, bb_file))
+        # print(rgb_video_path, rgb_start)
+        # define an empty array
+        video_rgb = np.zeros((desired_frames, height, width, 3))
+        joined_path = os.path.join(rgb_video_path, 'frame*.jpg')
+        image_path = sorted(glob.glob(joined_path))
+        # print(image_path) 
+        actual_frames = len(image_path)
+        # print(actual_frames, start_point)
+        if start_point == -1:
+            if actual_frames >= desired_frames:
+                # print(1)
+                evensList = list(range(0, desired_frames, 1))
+            else:
+                # print(2)
+                added_num = desired_frames - actual_frames
+                oldList = list(range(0, actual_frames, 1))
+                addedArray = np.random.choice(oldList, added_num)
+                addedList = addedArray.tolist()
+                evensList = oldList + addedList
+                evensList.sort()
+        else:
+            # print(3)
+            evensList = list(range(start_point, start_point + desired_frames*sample, sample))
+        # print(evensList)
+        image_path = sorted(glob.glob(joined_path))
+        for index in range(desired_frames):
+                # get the index from the number list
+                im_idx = evensList[index]
+                # get the frame path from the index
+                frame_idx = image_path[im_idx]
+                tmp_image = io.imread(frame_idx)
+                
+                bb = bb_tensor[im_idx, :].numpy()
+                # print('im_idx ', im_idx, ' ', 'bb: ', bb)
+                if bb[0]-10 >=0 and bb[2]-10 >=0 and bb[1]+10 <= tmp_image.shape[0] and bb[3]+10 <= tmp_image.shape[1]:
+                    tmp_image = tmp_image[bb[0]-10:bb[1]+10, bb[2]-10:bb[3]+10, :]
+                else:
+                    tmp_image = tmp_image[bb[0]:bb[1], bb[2]:bb[3], :]
+                
+                # print(im_idx, 'im_idx', tmp_image.shape, 'shape')
+                # scale pixel values in range(-1, 1)
+                tmp_image = (tmp_image/255.)*2 - 1
+                # resize each frame image into 256 x 256
+                tmp_image = resize(tmp_image, (height, width), mode='constant')
+                # print(tmp_image)
+                video_rgb[index, :, :, :] = tmp_image    
+        
+        return video_rgb
+
+        
+    def get_opt_video(self, opt_x_video_path, opt_y_video_path, opt_start, bb_dir, sub_N, stride, sample):
+        # define the number of frames needed
+        desired_frames = sub_N
+        start_point = int(opt_start)
+        # the following parameters are fixed for experiments
+        height = 224
+        width = 224
+        
+        # define an empty array
+        video_opt = np.zeros((desired_frames, height, width, 2))
+        joined_path_x = os.path.join(opt_x_video_path, 'frame*.jpg')
+        joined_path_y = os.path.join(opt_y_video_path, 'frame*.jpg')
+        image_x_path = sorted(glob.glob(joined_path_x))
+        image_y_path = sorted(glob.glob(joined_path_y))
+        
+        head, tail = os.path.split(opt_x_video_path)
+        bb_file = tail + '.pth'
+        bb_tensor = torch.load(os.path.join(bb_dir, bb_file))
+        
+        # print('--------', image_x_path)        
+        actual_frames = len(image_x_path)
+        # print('--------', actual_frames, start_point)
+        if start_point == -1:
+            if actual_frames >= desired_frames:
+                evensList = list(range(0, desired_frames, 1))
+            else:
+
+                added_num = desired_frames - actual_frames
+                oldList = list(range(0, actual_frames, 1))
+                addedArray = np.random.choice(oldList, added_num)
+                addedList = addedArray.tolist()
+                evensList = oldList + addedList
+                evensList.sort()
+        else:
+            evensList = list(range(start_point, start_point + desired_frames*sample, sample))
+                
+        # print(evensList)
+        image_x_path = sorted(glob.glob(joined_path_x))
+        image_y_path = sorted(glob.glob(joined_path_y))
+        for index in range(desired_frames):
+                # get the index from the number list
+                im_idx = evensList[index]
+                # combine x and y components for optic flow
+                one_frame = np.zeros((height, width, 2))
+                # get the frame path from the index
+                frame_x_idx = image_x_path[im_idx]
+                frame_y_idx = image_y_path[im_idx]
+                tmp_x_image = io.imread(frame_x_idx)
+                tmp_y_image = io.imread(frame_y_idx)
+                
+                bb = bb_tensor[im_idx, :].numpy()
+                if bb[0]-10 >=0 and bb[2]-10 >=0 and bb[1]+10 <= tmp_x_image.shape[0] and bb[3]+10 <= tmp_x_image.shape[1]:
+                    tmp_x_image = tmp_x_image[bb[0]-10:bb[1]+10, bb[2]-10:bb[3]+10]
+                    tmp_y_image = tmp_y_image[bb[0]-10:bb[1]+10, bb[2]-10:bb[3]+10]
+                else:
+                    tmp_x_image = tmp_x_image[bb[0]:bb[1], bb[2]:bb[3]]
+                    tmp_y_image = tmp_y_image[bb[0]:bb[1], bb[2]:bb[3]]
+                # scale the pixel values in range (-1, 1)
+                tmp_x_image = (tmp_x_image/255.)*2 - 1
+                tmp_y_image = (tmp_y_image/255.)*2 - 1
+                # resize each frame image into 256 x 256
+                tmp_x_image = resize(tmp_x_image, (height, width), mode='constant')
+                tmp_y_image = resize(tmp_y_image, (height, width), mode='constant')
+
+                one_frame[:, :, 0] = tmp_x_image
+                one_frame[:, :, 1] = tmp_y_image
+                video_opt[index, :, :, :] = one_frame
+        
+        return video_opt
+   
+    def left_right_flip(self, video):
+        # horizontal flip of the video
+        # RGB video left-right flip for each channel
+        # Optical flow video left-right flip for each channel, but
+        # also reverse the x components
+        frames = video.shape[0]
+        height, width = video.shape[1], video.shape[2]
+        channels = video.shape[3]
+        video_flipped = np.zeros((frames, height, width, channels))
+        for fi in range(frames):
+            # print(fi)
+            channel_im = np.zeros((height, width, channels))
+            for ci in range(channels):
+                flip_c = video[fi, :, :, ci]
+                temp_flip_c = np.flip(flip_c, 1)
+                if channels == 2 and ci == 0:
+                    temp_flip_c = -1 * temp_flip_c
+                channel_im[:, :, ci] = temp_flip_c
+            video_flipped[fi, :, :, :] = channel_im
+        return(video_flipped) 
---
+++ MPII_I3D_feat_ext/i3d_subseq_feature_ext_96.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from action_dataloader import ActionDataset, ToTensor, RandomCrop, CenterCrop
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+from pytorch_i3d import InceptionI3d
+import numpy as np
+import math
+from collections import OrderedDict
+import pandas as pd
+import os
+os.environ["CUDA_VISIABLE_DEVICES"]='0, 1, 2'
+
+class TwoStreamNet(nn.Module):
+    def __init__(self):
+        super(TwoStreamNet, self).__init__()
+        self.i3d_rgb = InceptionI3d(400, in_channels = 3)
+        self.i3d_opt = InceptionI3d(400, in_channels = 2)
+
+    def forward(self, x1, x2):
+        x_rgb = self.i3d_rgb(x1)
+        x_opt = self.i3d_opt(x2)
+        # print(x_rgb.shape)
+        # print(x_opt.shape)
+
+        return x_rgb, x_opt
+
+# define device: cpu at this stage
+# device = torch.device('cuda:0')
+
+#-------------------------------------------
+# weights initialization
+def weights_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.kaiming_uniform_(m.weight, a=math.sqrt(5))
+        if m.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(m.weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(m.bias, -bound, bound)
+    elif isinstance(m, nn.Conv1d):
+        nn.init.normal_(m.weight.data)
+        if m.bias is not None:
+            nn.init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        nn.init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            nn.init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        nn.init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            nn.init.normal_(m.bias.data)
+
+#--------------------------------------------
+
+#----------------------------------------------
+
+# define some parameters her
+
+# N is the batch size
+N = 20
+N_test = 20
+# C1 is the channels for rgb videos
+C1 = 3
+# C2 is the channels for opt videos
+C2 = 2
+# height and width for the input videos
+H, W = 224, 224
+
+# dropout_keep_prob=0.5
+
+num_epochs = 1
+#----------------------------------------------
+
+#----------------------------------------------
+# dataset
+tr_info_list = '/flush2/wan305/MPII_IJCV/mpii_info_list_with_subject_for_split_peter_96.csv'
+
+rgb_dir = '/datasets/work/D61_XTLAN_work/wan305/MPII_COOKING/rgb/'
+opt_dir = '/datasets/work/D61_XTLAN_work/wan305/MPII_COOKING/'
+bb_dir = '/datasets/work/D61_XTLAN_work/wan305/MPII_COOKING/mpii_bb_premium/'
+
+tr_hmdb51 = ActionDataset(tr_info_list, rgb_dir, opt_dir, bb_dir, mode = 'train', transforms=transforms.Compose([ToTensor()]))
+
+train_dataloader=DataLoader(tr_hmdb51,batch_size=N,shuffle=False, drop_last = False)
+
+#---------------------------------------------
+net = TwoStreamNet()
+net.i3d_rgb.load_state_dict(torch.load('/flush1/wan305/pretrained_I3D/rgb_imagenet.pt'))
+net.i3d_opt.load_state_dict(torch.load('/flush1/wan305/pretrained_I3D/flow_imagenet.pt'))
+
+net.cuda()
+net = nn.DataParallel(net)
+#---------------------------------------------
+
+for i in range(num_epochs):
+    # Testing---------------------------------
+    
+    net.eval()
+
+    with torch.no_grad():    
+        # x_f_tensor = torch.cuda.FloatTensor(1, 1024, 16, 7, 7).fill_(0)
+        # x_p_tensor = torch.cuda.FloatTensor(1, 1024, 15, 1, 1).fill_(0)
+        x_rgb_tensor = torch.cuda.FloatTensor(1, 400).fill_(0)
+        x_opt_tensor = torch.cuda.FloatTensor(1, 400).fill_(0)
+        for t, sample_batched in enumerate(train_dataloader):
+            # test the model
+            rgb_video = Variable(sample_batched['video_rgb'], requires_grad = False).float().cuda()
+            opt_video = Variable(sample_batched['video_opt'], requires_grad = False).float().cuda()
+            # bow = Variable(sample_batched['BOW'])
+            # add action labels (ground truth) for prediction pipeline
+            label = Variable(sample_batched['video_label'], requires_grad = False).cuda()
+        
+            x_rgb, x_opt = net(rgb_video, opt_video)
+            # print(x_rgb)
+            # print('------', x_opt)
+            # x_f_tensor = torch.cat((x_f_tensor, x_f), 0)
+            
+            # x_p_tensor = torch.cat((x_p_tensor, x_p), 0)
+            x_rgb_tensor = torch.cat((x_rgb_tensor, x_rgb), 0)
+            x_opt_tensor = torch.cat((x_opt_tensor, x_opt), 0)
+            # ind = t % 15
+            # if t > 0 and ind == 0:
+            #     x_f_tensor = x_f_tensor[1:, :, :, :, :]
+            #     filename = '/flush2/wan305/hmdb_s1_te_fusion_64_' + str(t) + '.pth'
+            #     torch.save(x_f_tensor, filename)
+            #     print('saved', t, x_f_tensor.shape)
+            #     x_f_tensor = torch.cuda.FloatTensor(1, 1024, 16, 7, 7).fill_(0)
+                                 
+            print(t, '-----------------')
+        # x_f_tensor = x_f_tensor[1:, :, :, :, :]
+        # filename = '/flush2/wan305/hmdb_s1_te_fusion_64_' + str(t) + '.pth'
+        # torch.save(x_f_tensor, filename)
+        # print('saved', t, x_f_tensor.shape)
+    
+        # x_p_tensor = x_p_tensor[1:, :, :, :, :]
+        x_rgb_tensor = x_rgb_tensor[1:, :]
+        x_opt_tensor = x_opt_tensor[1:, :]
+        print('x_rgb_tensor: ', x_rgb_tensor.shape)
+        print('x_opt_tensor: ', x_opt_tensor.shape)
+        # torch.save(x_p_tensor, '/flush2/wan305/hmdb_s1_te_pool_64.pth')
+        torch.save(x_rgb_tensor, '/flush2/wan305/MPII_IJCV/Raw_400D/mpii_pred_96_rgb.pth')
+        torch.save(x_opt_tensor, '/flush2/wan305/MPII_IJCV/Raw_400D/mpii_pred_96_opt.pth')
